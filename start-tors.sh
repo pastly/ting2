9b92d5bf2ea910078373fe585d13251c4822783b
Matt Traudt <sirmatt@ksu.edu>
2017-08-14 09:23:07 -0400

    Add hopefully helpful start-tors.sh

---
+++ start-tors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Helper script to start a number of Tor processes for mtdns to then use
+DD=$(pwd)/tordatadirs
+TOR_BIN="$HOME/src/tor/src/or/tor"
+mkdir $DD
+# SocksPorts will be whatever is listed in the for loop
+# ControlPorts will be +1 of SocksPorts
+for A in 12000 12002 12004 12006; do 
+    $TOR_BIN \
+        --SocksPort $A \
+        --ControlPort $(($A+1)) \
+        --CookieAuthentication 1 \
+        --Log "err file $DD/$A/error.log" \
+        --DataDirectory $DD/$A \
+        --PidFile $DD/$A/tor.pid \
+        --defaults-torrc /dev/null \
+        --LearnCircuitBuildTimeout 0 \
+        --CircuitBuildTimeout 10 \
+        --RunAsDaemon 1 \
+        --Bridge "127.0.0.1:9003 B28D5058E30620358B33D75BFB9F20192CF82270" \
+        --UseBridges 1 \
+        ;
+done
